ae1ccfa66457b251ad4bf6f7e1296170ccd93dcc
Brian Son <bson22@gmail.com>
2014-05-28 20:29:54 -0600

    adds focus and scrollto function

--- app/scripts/controllers/invoice.js
+++ app/scripts/controllers/invoice.js
@@ -9,8 +9,17 @@ angular.module('reqrentApp')
 
     $scope.addBill = function() {
   		$scope.invoiceList.push($scope.bill);
-  		$scope.bill = {};
-  		console.log($scope.invoiceList);
+  		$scope.bill={};
+  		$scope.focusBox();
+
+  		console.log($scope.invoiceList); //for debugging
   	}
+
+  	$scope.focusBox = function () {
+  		var scrollToElement = document.getElementById("selector")
+  		var focusOnElement = document.getElementById("text");
+  		focusOnElement.focus();
+  		scrollToElement.scrollIntoView();
+  	};
 
   });
