7d5efd4342bdc54aa9b2bd83ef2bf351569eaadf
Brian Son <bson22@gmail.com>
2014-05-31 12:38:35 -0600

    adds invoiceservice

--- app/scripts/controllers/invoice.js
+++ app/scripts/controllers/invoice.js
@@ -1,18 +1,19 @@
 'use strict';
 
 angular.module('reqrentApp')
-  .controller('InvoiceCtrl', function ($scope) {
+  .controller('InvoiceCtrl', function ($scope, Invoiceservice) {
 
-  	$scope.invoiceList = [];  //empty array for the invoiceList
+  	// $scope.invoiceList = [];  //empty array for the invoiceList
+    $scope.invoiceList = Invoiceservice.list;
 
   	$scope.bill = {};  //empty object to save bills
 
-    $scope.addBill = function() {               //addBill function adds a bill to the invoice list.
-  		$scope.invoiceList.unshift($scope.bill);
-  		$scope.bill={};
+    $scope.addBill = function() {               //addBill function adds a bill to the invoice list.	
+  		Invoiceservice.addBill($scope.bill);
+      $scope.bill={};
   		$scope.focusBox();
-      
-      console.log($scope.invoiceList); //for debugging
+
+      console.log($scope.invoiceList);
   	}
 
   	$scope.focusBox = function () {                                      //function selects and scrolls to the textfield so you don't have to use your mouse
--- app/scripts/services/invoiceservice.js
+++ app/scripts/services/invoiceservice.js
@@ -3,4 +3,16 @@
 angular.module('reqrentApp')
   .service('Invoiceservice', function Invoiceservice() {
     // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+   var invoiceList = [];
+
+   var addBill =  function (bill) {
+   		invoiceList.unshift(bill);
+   };
+
+   // Anythouhing returned in this object is available outside the service.
+   return { 
+   	addBill: addBill,
+   	list: invoiceList
+   };
+
+});
