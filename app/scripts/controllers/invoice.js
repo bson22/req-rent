1678db9bf8ac45fc73f6440a46edb11351ce4f9a
Brian Son <bson22@gmail.com>
2014-05-31 14:52:11 -0600

    adds styling

--- app/scripts/controllers/invoice.js
+++ app/scripts/controllers/invoice.js
@@ -8,18 +8,19 @@ angular.module('reqrentApp')
 
   	$scope.bill = {};  //empty object to save bills
 
+    $scope.total = 0;
+
     $scope.addBill = function() {               //addBill function adds a bill to the invoice list.	
   		Invoiceservice.addBill($scope.bill);
       $scope.bill={};
-  		$scope.focusBox();
-
+      $scope.focusBox();
       $scope.total = Invoiceservice.total();
 
       console.log($scope.total);
   	}
 
   	$scope.focusBox = function () {                                      //function selects and scrolls to the textfield so you don't have to use your mouse
-  		var scrollToElement = document.getElementById("selector")
+  		var scrollToElement = document.getElementById("text")
   		var focusOnElement = document.getElementById("text");
   		focusOnElement.focus();
   		scrollToElement.scrollIntoView();
